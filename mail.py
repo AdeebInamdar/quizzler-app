ac73ba53e40150f487caff4c08fe754a781c9f37
Adeeb Inamdar <126943685+AdeebInamdar@users.noreply.github.com>
2024-12-14 13:14:26 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -8,12 +8,12 @@ year = now.time()
 print(date)
 print(year)
 score = input("enter you score :")
-my_email = "adeebinamdar9@gmail.com"
-password = "xnmx wrrf ssqp pyap"
+my_email = "add your sending mail"
+password = "create password for google app"
 connection = smtplib.SMTP("smtp.gmail.com", port=587)
 connection.starttls()
 connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs="sudubiradar0609@gmail.com", msg=f"Subject:qizzler app\n\n {date}// {year} your score for the quiz is :{score}")
+connection.sendmail(from_addr=my_email, to_addrs="sender email", msg=f"Subject:qizzler app\n\n {date}// {year} your score for the quiz is :{score}")
 connection.close()
 
 
